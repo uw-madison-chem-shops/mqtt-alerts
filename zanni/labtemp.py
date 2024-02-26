65995dbe3d8b31cb317d44c2e9739cefd134ec44
Blaise Thompson <blaise@untzag.com>
2024-02-26 14:02:38 -0600

    add timestamp to test email

--- zanni/labtemp.py
+++ zanni/labtemp.py
@@ -1,3 +1,4 @@
+import time
 import smtplib
 from email.message import EmailMessage
 
@@ -5,7 +6,7 @@ msg = EmailMessage()
 msg["Subject"] = "Testing smtplib and email from Python."
 msg["From"] = "zannibot@chem.wisc.edu"
 msg["To"] = ",".join(["blaise.thompson@wisc.edu", "wjeong25@wisc.edu", "jagapen@wisc.edu"])
-msg.set_content("This is a test message. Foo, bar, baz.")
+msg.set_content(f"This is a test message. Foo, bar, baz. Time={time.time()}")
 
 with smtplib.SMTP_SSL("localhost") as s:
     s.send_message(msg)
