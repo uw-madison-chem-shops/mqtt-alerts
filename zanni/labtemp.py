6bbd3202783c889e4841c7753528b82919e8b542
wjeong25 <159492314+wjeong25@users.noreply.github.com>
2024-04-19 16:06:31 -0500

    Update labtemp.py (#1)
    When I ran it in my computer, client.loop_forever() part had an error as I intentionally triggered sending email(K_P_temp < 21 almost always is true, original threshold is K_P_temp < 20). I'll try to resolve for this issues or just make a simpler edit proposal where I don't apply the callback function. Thanks for your assistance Blaise!

--- zanni/labtemp.py
+++ zanni/labtemp.py
@@ -1,12 +1,44 @@
 import time
 import smtplib
 from email.message import EmailMessage
+from paho.mqtt.client import Client
+client = Client()
+client.connect("mqtt.chem.wisc.edu")
 
+client.subscribe("homie/zanni-weather-000000/#")
+client.subscribe("homie/zanni-weather-000001/#")
+
+
+img0="https://graphite.chem.wisc.edu/render/?width=586&height=308&target=zanni.weather.*.*.temperature, &from=-1d" #graph of temperature v time, from=-1d is for past day
 msg = EmailMessage()
-msg["Subject"] = "Testing smtplib and email from Python."
+msg["Subject"] = "Laser Room Abnormal Temperature Alert"
 msg["From"] = "zannibot@chem.wisc.edu"
-msg["To"] = ",".join(["blaise.thompson@wisc.edu", "wjeong25@wisc.edu", "jagapen@wisc.edu"])
-msg.set_content(f"This is a test message. Foo, bar, baz. Time={time.time()}")
+msg["To"] = ",".join(["wjeong25@wisc.edu"]) #Zanni group: zanni_group@chem.wisc.edu
 
-with smtplib.SMTP_SSL("localhost") as s:
-    s.send_message(msg)
+
+def callback(client, userdata, message):
+    # this is where you would put your code watching for trigger conditions
+    # and trigger your message to be sent (email, SMS...)
+    #Following if statement prints temperature of each room (and warning msg and graph if triggered)
+    if message.topic == "homie/zanni-weather-000000/bme280/temperature":
+        B_W_temp= float(message.payload.split(b' ')[0]) #trims down to just temperature value
+        print("Room temperature of Black and Wisconsin: ", B_W_temp)
+        if B_W_temp > 21 or B_W_temp < 19: #trigger condition, need to develop into email
+            msg.set_content(f"Temperature of laser room with Black and Wisconsin is outside the usual range. Please check the laser room at your earliest convenience. \nTime={time.asctime()} MQTT site: https://mqtt.chem.wisc.edu/zanni/ Temp graph:{img0}")
+            with smtplib.SMTP_SSL("localhost") as s:
+                s.send_message(msg) #sends email
+            time.sleep(3600) #when email is triggered, pauses for 1 hr (to minimize spamming mails)
+    elif message.topic == "homie/zanni-weather-000001/bme280/temperature":
+        K_P_temp= float(message.payload.split(b' ')[0]) #trims down to just temperature value
+        print("Room temperature of Kickapoo and Pecatonica: ", K_P_temp)
+        if K_P_temp > 22 or K_P_temp < 21: #trigger condition 2, need to develop into email
+            msg.set_content(f"Temperature of laser room with Kickapoo and Pecatonica is outside the usual range. Please check the laser room at your earliest convenience. \nTime={time.asctime()} MQTT site: https://mqtt.chem.wisc.edu/zanni/ Temp graph:{img0}")
+            with smtplib.SMTP_SSL("localhost") as s:
+                s.send_message(msg) #sends email
+            time.sleep(3600) #when email is triggered, pauses for 1 hr (to minimize spamming mails)
+    else:
+        pass
+    
+client.on_message = callback
+
+client.loop_forever()
